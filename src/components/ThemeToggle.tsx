044eb70e672ba9d29a838ea29b0423d62d362908
AslamtoIbrahim <aslamtoibrahim@gmail.com>
2025-07-24 18:47:09 +0100

    feat(theme): add dark mode to the whole app

--- src/components/ThemeToggle.tsx
+++ src/components/ThemeToggle.tsx
@@ -4,13 +4,27 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { useState } from "react";
 
 const ThemeToggle = () => {
-  const [moonIcon, setMoonIcon] = useState(true);
-  const switchMoon = () => {
-    setMoonIcon(prev => !prev)
+  const preferDark = () =>
+    window.matchMedia("(prefers-color-scheme: dark)").matches;
+  const [isDark, setIsDArk] = useState(() => {
+    return (
+      localStorage.theme === "dark" ||
+      (!("theme" in localStorage) && preferDark())
+    );
+  });
+
+  const toggleTheme = () => {
+    const newMode = !isDark;
+    document.documentElement.classList.toggle("dark", newMode);
+    setIsDArk(newMode);
+    localStorage.theme = newMode ? "dark" : "";
   };
   return (
-    <div className="flex items-center gap-2 cursor-pointer" onClick={switchMoon}>
-      <FontAwesomeIcon icon={moonIcon ? faMoon : solidMoon} />
+    <div
+      className="flex items-center gap-2 cursor-pointer"
+      onClick={toggleTheme}
+    >
+      <FontAwesomeIcon icon={isDark ? solidMoon : faMoon} />
       <p className="font-sans capitalize">Dark mode</p>
     </div>
   );
